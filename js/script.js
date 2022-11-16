a29707780b45ff2b4b87e9c0cb52d6332ebb4e21
Bellatrix-Salungweni1 <bellatrixsalungweni106@student.unsrat.ac.id>
2022-11-16 19:14:05 +0800

    Mengatur tampilan responsif dan btn back to top

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const humbergerMenuToggle = document.querySelector('.header-burger-menu');
 const menu = document.querySelector('.header-menu');
 const btnHero = document.querySelector('.btn-hero');
+const btnHero1 = document.querySelector('.btn-hero1')
 const projectLayer = document.querySelector('#project-layer');
 const project = document.querySelector('#project');
 
@@ -16,3 +17,25 @@ projectLayer.addEventListener('click', (e) => {
     projectLayer.classList.remove('active');
     project.classList.remove('active');
 })
+btnHero1.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+    projectLayer.classList.add('active');
+    project.classList.add('active');
+})
+
+let topButton = document.getElementById("topBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
