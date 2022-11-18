74d2791cd77c0e8c2e823192a3884d0313ed198a
Rizky-AR <alvarezrez6@gmail.com>
2022-11-18 11:05:38 +0700

    Revisi section service, menambahkan slider, dan merapikan struktur html

--- js/script.js
+++ js/script.js
@@ -39,3 +39,34 @@ function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 }
+
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  sliderPerGroup: 3,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    540: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
